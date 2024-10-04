6c6c34ad017559a880932b0753e67e2091421713
SaiiintCHain <zok-hor@mail.ru>
2024-10-04 18:30:34 +0300

    Update proizvolnoe_chislo_params.py

--- proizvolnoe_chislo_params.py
+++ proizvolnoe_chislo_params.py
@@ -7,8 +7,10 @@ def single_root_words(root_word, *other_words):
             same_words.append(other_words[i])
         if other_words[i] in root_word:
             same_words.append(other_words[i])
-    print(same_words)
+    return(same_words)
 
 
 result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
 result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print(result1)
+print(result2)
